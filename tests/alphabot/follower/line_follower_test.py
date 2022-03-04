7d257f7685e52159ca48c644d32888054b34e2da
Nikolay Makarov <makarov-na@yandex.ru>
2022-03-05 00:14:24 +0300

    Removed broken test

--- tests/alphabot/follower/line_follower_test.py
+++ tests/alphabot/follower/line_follower_test.py
@@ -22,19 +22,3 @@ class TestLineFollower(unittest.TestCase):
         # THEN
         self.assertEqual(0.01, dt1 / 1000)
         self.assertEqual(0.01, dt2 / 1000)
-
-    def test_calculate_delta_time_mcs(self):
-        # GIVEN
-        line_follower = LineFollower(gpio=MagicMock())
-        line_follower._calculateDeltaTimeInMcs()
-
-        # WHEN
-        time.sleep(1/1_000_000)
-        dt1 = line_follower._calculateDeltaTimeInMcs()
-
-        time.sleep(1/1_000_000)
-        dt2 = line_follower._calculateDeltaTimeInMcs()
-
-        # THEN
-        self.assertEqual(1, dt1)
-        self.assertEqual(1, dt2)
