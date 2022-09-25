bd69544ff408e939b314f1b54a22122188ab3f32
Nikolay Makarov <makarov-na@yandex.ru>
2022-09-25 21:28:48 +0300

    #16 logging level configuration example and speed limit 18

--- src/alphabot/follower/config_module.py
+++ src/alphabot/follower/config_module.py
@@ -5,7 +5,7 @@ class LineFollowerConfig:
         self.KD = 20
         self.KI = 0
         self.MAX_OUT = 60
-        self.SPEED_POWER = 20
+        self.SPEED_POWER = 18
         self.SLEEP_TIME = 1 / 1_000_000 * 10
         self.TARGET_VALUE_LEFT = 0
         self.TARGET_VALUE_RIGHT = 0
--- src/main.py
+++ src/main.py
@@ -5,4 +5,5 @@ from alphabot.hardware.gpio_module import GpioWrapper
 logging.basicConfig(level=logging.INFO)
 follower = LineFollower(gpio=GpioWrapper())
 follower.logger.setLevel(level=logging.INFO)
+# logging.getLogger('alphabot.truck_module').setLevel(level=logging.ERROR)
 follower.startFollowing()
