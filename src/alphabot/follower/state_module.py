46135a203af9f0c37849f424e25f3412aaec8eb7
Nikolay Makarov <makarov-na@yandex.ru>
2022-11-26 17:22:20 +0300

    #16 State test and fix

--- src/alphabot/follower/state_module.py
+++ src/alphabot/follower/state_module.py
@@ -20,16 +20,21 @@ class TurnRightAngle(State):
         if self._start_pose is None:
             self._start_pose = event.pose
             self._truck.stop()
-
         if event.pose == Pose.OUT_OF_LINE:
-            return LineSearch(self._truck).doAction(event)
+            return self._createLineSearch(event)
         if event.pose == Pose.ON_LINE_WITH_TREE_CENTRAL_SENSORS:
-            LineFollow(self._truck).doAction(event)
+            return self._createLineFollow(event)
         if self._start_pose == Pose.ON_RIGHT_TURN:
             self._truck.rotateAroundRightWheel(20)
         else:
             self._truck.rotateAroundLeftWheel(20)
         return self
+
+    def _createLineFollow(self, event):
+        LineFollow(self._truck).doAction(event)
+
+    def _createLineSearch(self, event):
+        return LineSearch(self._truck).doAction(event)
 
 
 class LineFollow(State):
--- tests/alphabot/follower/state_test.py
+++ tests/alphabot/follower/state_test.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 
 from alphabot.follower.event_module import Event
 from alphabot.follower.pose_detector_module import Pose
-from alphabot.follower.state_module import Init, Stop, LineFollow, LineSearch
+from alphabot.follower.state_module import Init, Stop, LineFollow, LineSearch, TurnRightAngle
 
 
 class TestState(unittest.TestCase):
@@ -50,4 +50,34 @@ class TestState(unittest.TestCase):
 
         # THEN
         self.assertIsInstance(state, Stop)
+        track.stop.assert_called_once()
+
+    def test_turn_right_angle_state_out_of_line(self):
+        # GIVEN
+        track = MagicMock()
+        track.stop = MagicMock()
+        state = TurnRightAngle(track)
+        state._createLineSearch = MagicMock()
+        state._createLineSearch.return_value = MagicMock()
+
+        # WHEN
+        outState = state.doAction(Event(Pose.OUT_OF_LINE, [100, 0, 0, 0, 100]))
+
+        # THEN
+        self.assertEqual(outState, state._createLineSearch.return_value)
+        track.stop.assert_called_once()
+
+    def test_turn_right_angle_state_on_line(self):
+        # GIVEN
+        track = MagicMock()
+        track.stop = MagicMock()
+        state = TurnRightAngle(track)
+        state._createLineFollow = MagicMock()
+        state._createLineFollow.return_value = MagicMock()
+
+        # WHEN
+        outState = state.doAction(Event(Pose.ON_LINE_WITH_TREE_CENTRAL_SENSORS, [100, 0, 0, 0, 100]))
+
+        # THEN
+        self.assertEqual(outState, state._createLineFollow.return_value)
         track.stop.assert_called_once()
