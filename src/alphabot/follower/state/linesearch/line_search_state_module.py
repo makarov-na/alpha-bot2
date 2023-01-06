61381e64055520fbdc6c984e4c636df049f9c833
Nikolay Makarov <makarov-na@yandex.ru>
2023-01-06 13:29:14 +0300

    #16 Line search naive method

--- src/alphabot/follower/state/linesearch/line_search_state_module.py
+++ src/alphabot/follower/state/linesearch/line_search_state_module.py
@@ -1,17 +1,35 @@
+import time
+
 from alphabot.bot.truck_module import Truck
 from alphabot.follower.event.event_module import Event
+from alphabot.follower.pose.pose_detector_module import Pose
 from alphabot.follower.state.base_state_module import State
 import alphabot.follower.state.stop.stop_state_module as stm
+import alphabot.follower.state.linefollow.line_follow_state_module as lfm
 
 
 class LineSearch(State):
+    ACTION_TIMEOUT_SEC = 5
+    TURN_POWER = 40
 
     def __init__(self, truck: Truck) -> None:
         self._truck = truck
-        self._limit = 0
+        self._init_time = None
 
     def doAction(self, event: Event) -> State:
-        if self._limit <= 0:
-            return stm.Stop(self._truck).doAction(event)
-        # TODO Search line logic with limits must be placed here
+        if self._init_time is None:
+            self._truck.powerStop()
+            self._init_time = time.time()
+        action_time = time.time() - self._init_time
+        if action_time >= LineSearch.ACTION_TIMEOUT_SEC:
+            return self._createStopState(event)
+        if event.pose in (Pose.ON_LINE_WITHOUT_CENTRAL_SENSOR, Pose.ON_LINE_WITH_CENTRAL_SENSOR):
+            return self._createLineFollowerState(event)
+        self._truck.setTurnPower(LineSearch.TURN_POWER)
         return self
+
+    def _createLineFollowerState(self, event):
+        return lfm.LineFollow(self._truck).doAction(event)
+
+    def _createStopState(self, event):
+        return stm.Stop(self._truck).doAction(event)
--- tests/alphabot/follower/state/linesearch/line_search_state_test.py
+++ tests/alphabot/follower/state/linesearch/line_search_state_test.py
@@ -1,1 +1,53 @@
-# TODO wtite tests
+import time
+import unittest
+from unittest.mock import MagicMock, call
+
+from alphabot.follower.event.event_module import Event
+from alphabot.follower.pose.pose_detector_module import Pose
+from alphabot.follower.state.linefollow.line_follow_state_module import LineFollow
+from alphabot.follower.state.linesearch.line_search_state_module import LineSearch
+from alphabot.follower.state.stop.stop_state_module import Stop
+
+
+class TestState(unittest.TestCase):
+
+    def test_search_line_before_timeout_out_of_line(self):
+        # GIVEN
+        state = LineSearch(MagicMock())
+        state._truck = MagicMock()
+        state._truck.powerStop = MagicMock()
+        state._truck.setTurnPower = MagicMock()
+
+        # WHEN
+        outState = state.doAction(Event(Pose.OUT_OF_LINE, []))
+        outState = state.doAction(Event(Pose.OUT_OF_LINE, []))
+
+        # THEN
+        self.assertEqual(outState, state)
+        state._truck.powerStop.assert_called_once()
+        state._truck.setTurnPower.assert_has_calls([call(state.TURN_POWER), call(state.TURN_POWER)])
+
+    def test_search_line_after_timeout_out_of_line(self):
+        # GIVEN
+        state = LineSearch(MagicMock())
+        state._init_time = time.time() - LineSearch.ACTION_TIMEOUT_SEC
+        state._createStopState = MagicMock()
+        state._createStopState.return_value = Stop(Event(Pose.OUT_OF_LINE, []))
+
+        # WHEN
+        outState = state.doAction(Event(Pose.OUT_OF_LINE, []))
+
+        # THEN
+        self.assertEqual(outState, state._createStopState.return_value)
+
+    def test_search_line_before_timeout_on_line(self):
+        # GIVEN
+        state = LineSearch(MagicMock())
+        state._createLineFollowerState = MagicMock()
+        state._createLineFollowerState.return_value = LineFollow(Event(Pose.OUT_OF_LINE, []))
+
+        # WHEN
+        outState = state.doAction(Event(Pose.ON_LINE_WITHOUT_CENTRAL_SENSOR, []))
+
+        # THEN
+        self.assertEqual(str(LineFollow(MagicMock()).__class__), str(outState.__class__))
