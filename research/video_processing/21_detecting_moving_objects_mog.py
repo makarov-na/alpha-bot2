5953dcbd4988e37570b6430f6646da63a1b55750
Nikolay Makarov <makarov-na@yandex.ru>
2023-05-07 18:54:15 +0300

    #21 Prototype. Maximum countur selection.

--- research/video_processing/21_detecting_moving_objects_mog.py
+++ research/video_processing/21_detecting_moving_objects_mog.py
@@ -2,7 +2,8 @@ import time
 
 import cv2
 
-cap = cv2.VideoCapture("./video_camera_on_line.h264")
+#cap = cv2.VideoCapture("./video_camera_on_line.h264")
+cap = cv2.VideoCapture("/home/mna/Projects/Youtube/12_camera-video/line_follow.h264")
 
 success, original_frame = cap.read()
 
@@ -12,12 +13,11 @@ frame_width = int(original_frame.shape[1] * scale_percent / 100)
 frame_height = int(original_frame.shape[0] * scale_percent / 100)
 dir = '/home/mna/tmp/bot-video/camera-video/'
 
-mask_for_black_colour_stream = cv2.VideoWriter(dir+'mask_for_black_colour.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-eroded_mask_stream = cv2.VideoWriter(dir+'eroded_mask.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-dilated_mask_stream = cv2.VideoWriter(dir+'dilated_mask.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-bgr_mask_stream = cv2.VideoWriter(dir+'bgr_mask.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-resized_frame_stream = cv2.VideoWriter(dir+'resized_frame.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-
+mask_for_black_colour_stream = cv2.VideoWriter(dir + 'mask_for_black_colour.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (frame_width, frame_height))
+eroded_mask_stream = cv2.VideoWriter(dir + 'eroded_mask.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (frame_width, frame_height))
+dilated_mask_stream = cv2.VideoWriter(dir + 'dilated_mask.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (frame_width, frame_height))
+bgr_mask_stream = cv2.VideoWriter(dir + 'bgr_mask.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (frame_width, frame_height))
+resized_frame_stream = cv2.VideoWriter(dir + 'resized_frame.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (frame_width, frame_height))
 
 while success:
 
@@ -29,23 +29,23 @@ while success:
 
     # Выбираем диапазон для фильтрации
     black_color_low = (0, 0, 0)
-    black_color_high = (255, 255, 130)
+    black_color_high = (255, 255, 110)
     mask_for_black_colour = cv2.inRange(hsv_frame, black_color_low, black_color_high)
-    mask_for_black_colour_stream.write(mask_for_black_colour)
+    mask_for_black_colour_stream.write(cv2.cvtColor(mask_for_black_colour, cv2.COLOR_GRAY2BGR))
     cv2.imshow('mask_for_black_colour', mask_for_black_colour)
-
-
 
     # Вытравливаем мелкие элементы для устранения шума.
     erode_kernel = cv2.getStructuringElement(cv2.MORPH_ERODE, (50, 50))
     eroded_mask = mask_for_black_colour.copy()
     cv2.erode(mask_for_black_colour, erode_kernel, eroded_mask)
+    eroded_mask_stream.write(cv2.cvtColor(eroded_mask, cv2.COLOR_GRAY2BGR))
     cv2.imshow('eroded_mask', eroded_mask)
 
     # Выполняем заливку оставшихся элементов для устранения избыточного вытравливания
     dilate_kernel = cv2.getStructuringElement(cv2.MORPH_DILATE, (60, 60))
     dilated_mask = eroded_mask.copy()
     cv2.dilate(eroded_mask, dilate_kernel, dilated_mask)
+    dilated_mask_stream.write(cv2.cvtColor(dilated_mask, cv2.COLOR_GRAY2BGR))
     cv2.imshow('dilated_mask', dilated_mask)
 
     # Находим на маске контуры
@@ -55,17 +55,17 @@ while success:
     # Для визуализации переводим маску в RGB и рисуем контур
     bgr_mask = cv2.cvtColor(dilated_mask, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(bgr_mask, contours, -1, (0, 255, 0), 3)
+    bgr_mask_stream.write(bgr_mask)
     cv2.imshow('bgr_mask', bgr_mask)
 
     # Рисуем контуры на оригинальной картинке
     cv2.drawContours(resized_frame, contours, -1, (0, 255, 0), 3)
 
-    # Находим
+    # Находим контуры и берем самый большой по площади
     if len(contours) > 0:
-        cnt = contours[0]
+        cnt = max(contours, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(cnt)
         cv2.rectangle(resized_frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
-
         deviation = x - (frame_width - (x + w))
         cv2.putText(resized_frame, "Deviation: " + str(deviation), (0, 0 + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
